5b8f70caa507eb7bffe09a6da4d1ca4d1e0c5913
Jake M <jakemcgdev@protonmail.com>
2024-11-30 08:08:52 -0800

    Create 2024 Svelte project

---
+++ 2024/svelte/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Advent of Code 2024</h1>
+<p>
+    Welcome to my <a href="https://adventofcode.com/2024">Advent of Code 2024</a> project. 
+    The solutions here are solved using SvelteKit.
+</p>
+<h2>Days</h2>
+<ul>
+    <li>
+
+    </li>
+</ul>
